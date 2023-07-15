5840ebc3be5217c778f867f07bb498eefec21c78
Ditra Amadia <ditrarizqaamadia@gmail.com>
2023-07-15 19:19:45 +0700

    server setup

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://127.0.0.1:27017/wikiDB");
+
+const articleSchema = {
+    title: String,
+    content: String
+}
+const Article = mongoose.model("Article", articleSchema);
+
+app.listen(3000, () => {
+    console.log("Server is started on port 3000");
+})
